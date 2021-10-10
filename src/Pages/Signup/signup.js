7aa8d5def8be6f4f860ca831c6326e588d0024f4
gadaharsh <gada.hp@somaiya.edu>
2021-10-10 18:16:04 +0530

    signup UI  additional chanegs

--- src/Pages/Signup/signup.js
+++ src/Pages/Signup/signup.js
@@ -3,10 +3,41 @@ import './signup.css';
 
 function Signup() {
     return (
-        <div className="icon-text">
-            <h4>Hello, Signup page of playmate ! 🧨🎇🧨</h4>
+        <div className="main-div" >
+            <h2>Be a part of the exclusive PlayMate Community </h2>
+
+            <form className="center">
+                <div class="col">
+                    <label>
+                        <input placeholder="Enter your Name" tabindex="1" />
+                    </label>
+                </div>
+
+                <div class="col">
+                    <label>
+                        <input placeholder="Enter your Contact Number" tabindex="3" />
+                    </label>
+                </div>
+
+                {/* <div class="col">
+                    <fieldset>
+                        <input id="male" type="radio" name="choice1" value="Male" />
+                        <label for="male" tabindex="5">Male</label>
+                        <input id="female" type="radio" name="choice2" value="Female" />
+                        <label for="female" tabindex="6">Female</label>
+                        <input id="others" type="radio" name="choice3" value="Others" />
+                        <label for="others" tabindex="7">Others</label>
+                    </fieldset>
+                </div> */}
+
+                <div class="col-submit">
+                    <button type="submit">Submit</button>
+                </div>
+
+            </form>
+
         </div>
-        
+
     )
 }
 
