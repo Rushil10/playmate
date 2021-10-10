1cbacdb1d04bee7dcfe85277fe6a80cad6cdf1d3
gadaharsh <gada.hp@somaiya.edu>
2021-10-10 13:14:09 +0530

    signup.js page added

--- src/App.js
+++ src/App.js
@@ -2,14 +2,19 @@ import logo from "./logo.svg";
 import "./App.css";
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 import Home from "./Pages/Home/home";
+import Signup from "./Pages/Signup/signup";
+
 
 function App() {
   return (
     <div className="App">
       <Router>
         <Switch>
-          <Route path="/">
+          <Route exact path="/">
             <Home />
+          </Route>
+          <Route path="/signup">
+            <Signup />
           </Route>
         </Switch>
       </Router>
---
+++ src/Pages/Signup/signup.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function Signup() {
+    return (
+        <div>
+            <h4>Hello, Signup page of playmate ! 🧨🎇🧨</h4>
+        </div>
+    )
+}
+
+export default Signup;
