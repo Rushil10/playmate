37fc9b5f79e3a386ab8c8944ee1633869cf0c878
Rushil10 <rushil.10.shah@gmail.com>
2021-10-10 12:20:25 +0530

    dom-setup

---
+++ src/Pages/Home/home.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function Home() {
+    return (
+        <div>
+            <h4>Hello, The landing page of playmate ! 🧨🎇🧨</h4>
+        </div>
+    )
+}
+
+export default Home;
