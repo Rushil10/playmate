05fabcf325bf3b9b4b27d6e38cea060d62859164
Rushil10 <rushil.10.shah@gmail.com>
2021-10-12 13:12:10 +0530

    signup page

--- src/App.js
+++ src/App.js
@@ -2,9 +2,8 @@ import logo from "./logo.svg";
 import "./App.css";
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 import Home from "./Pages/Home/home";
-import { initializeApp } from 'firebase/app';
-import Signup from "./Pages/Signup/signup";
-import Signup2 from './Pages/Signup2/signup2'
+import { initializeApp } from "firebase/app";
+import Signup2 from "./Pages/Signup2/signup2";
 
 const firebaseConfig = {
   apiKey: "AIzaSyBlz4RUWLWoNRcoqwSXtZCZtvpABQ6eY-k",
@@ -12,7 +11,7 @@ const firebaseConfig = {
   projectId: "playmate-29949",
   storageBucket: "playmate-29949.appspot.com",
   messagingSenderId: "616087980787",
-  appId: "1:616087980787:web:3d9110379df7d776268586"
+  appId: "1:616087980787:web:3d9110379df7d776268586",
 };
 
 // Initialize Firebase
@@ -26,10 +25,7 @@ function App() {
           <Route exact path="/">
             <Home />
           </Route>
-          {/* <Route path="/signup">
-            <Signup />
-          </Route> */}
-          <Route path="/signup2">
+          <Route path="/signup">
             <Signup2 />
           </Route>
         </Switch>
--- src/Pages/Signup2/signup2.js
+++ src/Pages/Signup2/signup2.js
@@ -2,10 +2,77 @@ import React, { useState, useMemo } from "react";
 import PhoneInput from "react-phone-input-2";
 import "react-phone-input-2/lib/style.css";
 import "./signup2.css";
-import logo from '../../images/logo.jpg'
+import logo from "../../images/logo.jpg";
+import Button from "@mui/material/Button";
+import {
+  getAuth,
+  RecaptchaVerifier,
+  signInWithPhoneNumber,
+} from "firebase/auth";
+import { CircularProgress } from "@mui/material";
 
 function Signup() {
   const [phone, setPhone] = useState("");
+  const [name, setName] = useState("");
+  const [otpSendLoading, setOtpLoading] = useState(false);
+
+  React.useEffect(() => {
+    const auth = getAuth();
+    window.recaptchaVerifier = new RecaptchaVerifier(
+      "sign-in-button",
+      {
+        size: "invisible",
+        callback: (response) => {
+          // reCAPTCHA solved, allow signInWithPhoneNumber.
+          onSignInSubmit();
+        },
+      },
+      auth
+    );
+  }, []);
+
+  const onSignInSubmit = (e) => {
+    //e.preventDefault();
+    setOtpLoading(true);
+    const auth = getAuth();
+    const phoneNumber = "+91" + phone;
+    const appVerifier = window.recaptchaVerifier;
+    console.log("hmmmm");
+    signInWithPhoneNumber(auth, phoneNumber, appVerifier)
+      .then((confirmationResult) => {
+        // SMS sent. Prompt user to type the code from the message, then sign the
+        // user in with confirmationResult.confirm(code).
+        console.log("otp has been sent !");
+        window.confirmationResult = confirmationResult;
+        setOtpLoading(false);
+        // ...
+      })
+      .catch((error) => {
+        console.log("Otp not sent !");
+        // Error; SMS not sent
+        // ...
+        setOtpLoading(false);
+      });
+  };
+
+  const onPressSignup = () => {
+    setOtpLoading(true);
+    console.log("heree");
+    console.log(phone);
+    console.log(name);
+    onSignInSubmit();
+  };
+
+  function handleChangeName(e) {
+    console.log(e.target.value);
+    setName(e.target.value);
+  }
+
+  function handleChangePhone(e) {
+    console.log(e.target.value);
+    setPhone(e.target.value);
+  }
+
   return (
     <div className="signupContainer">
       <div className="signupSpace">
@@ -14,18 +81,41 @@ function Signup() {
         </div>
         <div class="Input">
           <input
+            value={name}
+            onChange={handleChangeName}
             type="text"
-            id="input"
+            id="name"
             class="Input-text"
             placeholder="Your first name"
           ></input>
         </div>
-        <PhoneInput
+        <div class="Input">
+          <input
+            onChange={handleChangePhone}
+            type="text"
+            id="contact"
+            class="Input-text"
+            placeholder="10 digit Mobile Number"
+          ></input>
+        </div>
+        <div style={{ marginTop: 25 }}>
+          <Button
+            onClick={onPressSignup}
+            color="success"
+            fullWidth
+            variant="outlined"
+            id="sign-in-button"
+            disabled={otpSendLoading}
+          >
+            SEND OTP
+          </Button>
+        </div>
+        {/* <PhoneInput
           containerClass="phoneContainer"
           country={"in"}
           value={phone}
           onChange={(phone) => setPhone(phone)}
-        />
+        /> */}
       </div>
     </div>
   );
