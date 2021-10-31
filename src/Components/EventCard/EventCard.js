baf3fde76f9465069f83874dd3572a3b9ad3caef
Rushil10 <rushil.10.shah@gmail.com>
2021-10-31 18:01:35 +0530

    home screen

--- src/Components/EventCard/EventCard.js
+++ src/Components/EventCard/EventCard.js
@@ -1,8 +1,28 @@
-import { Grid, Paper, Typography } from "@mui/material";
+import { Button, Grid, Paper, Typography } from "@mui/material";
 import moment from "moment";
 import React from "react";
 import location from "../../images/location.png";
 import schedule from "../../images/schedule.png";
+import chronometer from "../../images/chronometer.png";
+import { getTimeFromDuration } from "../../util/functions";
+import useWindowDimensions from "../useWindowDimensions";
+import { styled } from "@mui/material/styles";
+import LinearProgress, {
+  linearProgressClasses,
+} from "@mui/material/LinearProgress";
+
+const BorderLinearProgress = styled(LinearProgress)(({ theme }) => ({
+  height: 10,
+  borderRadius: 5,
+  [`&.${linearProgressClasses.colorPrimary}`]: {
+    backgroundColor:
+      theme.palette.grey[theme.palette.mode === "light" ? 200 : 800],
+  },
+  [`& .${linearProgressClasses.bar}`]: {
+    borderRadius: 5,
+    backgroundColor: theme.palette.mode === "light" ? "#0bda51" : "#308fe8",
+  },
+}));
 
 function EventCard(props) {
   //console.log(props.item);
@@ -23,50 +43,120 @@ total_players: 21
 venue: "Cricket Grnd Open"
 __v: 0
 _id: "617b806be6a2a45eba9860a2" */
+  const { height, width } = useWindowDimensions();
   return (
-    /* <Paper borderBottomWidth="0px" borderRadius="15px" elevation={2}> */
     <Grid
       container
       borderRadius="15px"
       marginTop="9px"
       border="0.5px solid #d4d4d4"
-      padding="15px"
+      padding="9px"
+      paddingLeft="15px"
+      paddingBottom="9px"
     >
-      <Grid container alignItems="center" spacing={2.5}>
-        <Grid item>
-          <Typography variant="h6">{props.item.sport}</Typography>
+      <Grid container xs={12} md={7} order={{ xs: 1, sm: 1 }}>
+        <Grid container alignItems="center" spacing={2.5}>
+          <Grid item>
+            <Typography variant="h6">{props.item.sport}</Typography>
+          </Grid>
+          <Grid item>
+            <Typography variant="subtitle">
+              ( Avg Age : {props.item.age} )
+            </Typography>
+          </Grid>
         </Grid>
-        <Grid item>
+        <Grid
+          container
+          textAlign="center"
+          alignItems="center"
+          marginTop="0px"
+          spacing={1}
+        >
+          <Grid item display="flex" flexDirection="row">
+            <img
+              alt="venue"
+              src={location}
+              style={{ height: 21, width: 21, marginRight: 9 }}
+            />
+            <Typography variant="body1">{props.item.venue}</Typography>
+          </Grid>
+        </Grid>
+        <Grid
+          container
+          textAlign="center"
+          alignItems="center"
+          marginTop="0px"
+          spacing={1}
+        >
+          <Grid item display="flex" flexDirection="row">
+            <img
+              alt="schedule"
+              src={schedule}
+              style={{ height: 21, width: 21, marginRight: 9 }}
+            />
+            <Typography variant="body1">
+              {moment(props.item.day).format("dddd  DD  MMM ")}
+            </Typography>
+          </Grid>
+          <Grid item display="flex" flexDirection="row">
+            <img
+              alt="schedule"
+              src={chronometer}
+              style={{ height: 21, width: 21, marginRight: 9 }}
+            />
+            <Typography variant="body1">
+              {moment(props.item.timings).format("h:mm a")} -{" "}
+              {getTimeFromDuration(props.item.timings, props.item.duration)}
+            </Typography>
+          </Grid>
+        </Grid>
+      </Grid>
+      <Grid
+        container
+        alignItems="flex-end"
+        justifyContent="flex-end"
+        marginTop="5px"
+        order={{ xs: 3, sm: 2 }}
+        xs={12}
+        md={5}
+      >
+        <Grid item textAlign="center" xs={4} md={7}>
+          <Typography style={{ fontSize: 16.5 }} variant="title">
+            Price : ₹ {props.item.price_per_person}
+          </Typography>
+        </Grid>
+        <Grid item alignItems="center" textAlign="center" xs={4} md={7}>
+          <Button size="small" variant="contained">
+            {width < 450
+              ? "Join"
+              : width < 650 && width > 600
+              ? "Join"
+              : "Book My Spot"}
+          </Button>
+        </Grid>
+        <Grid item textAlign="center" xs={4} md={7}>
           <Typography variant="subtitle">
-            ( Avg Age : {props.item.age} )
+            ({props.item.rem_players} spots left)
           </Typography>
         </Grid>
       </Grid>
-      <Grid container textAlign="center" alignItems="center">
-        <img
-          alt="venue"
-          src={location}
-          style={{ height: 21, width: 21, marginRight: 9 }}
+      <Grid
+        item
+        order={{ xs: 2, sm: 3 }}
+        xs={12}
+        marginTop="9px"
+        marginBottom="9px"
+      >
+        <BorderLinearProgress
+          variant="determinate"
+          value={
+            ((props.item.total_players - props.item.rem_players) /
+              props.item.total_players) *
+            100
+          }
         />
-        <Grid item>
-          <Typography variant="body1">{props.item.venue}</Typography>
-        </Grid>
-      </Grid>
-      <Grid container textAlign="center" alignItems="center" marginTop="5px">
-        <img
-          alt="schedule"
-          src={schedule}
-          style={{ height: 21, width: 21, marginRight: 9 }}
-        />
-        <Grid item>
-          <Typography variant="body1">
-            {moment(props.item.day).format("dddd DD MMM ")}
-            {moment(props.item.timings).format("h:mm a")}
-          </Typography>
-        </Grid>
       </Grid>
     </Grid>
-    /* </Paper> */
   );
 }
 
--- src/util/functions.js
+++ src/util/functions.js
@@ -1,1 +1,26 @@
-export const getTimeFromDuration = (startTime, duration) => {};
+import moment from "moment";
+
+export const getTimeFromDuration = (startTime, duration) => {
+  var newDateObj = "Greater Than 3 hours";
+  if (duration === "30 min") {
+    newDateObj = moment(startTime).add(30, "m").toDate();
+    return moment(newDateObj).format("h:mm a");
+  } else if (duration === "1 hour") {
+    newDateObj = moment(startTime).add(60, "m").toDate();
+    return moment(newDateObj).format("h:mm a");
+  } else if (duration === "1 hour 30 min") {
+    newDateObj = moment(startTime).add(90, "m").toDate();
+    return moment(newDateObj).format("h:mm a");
+  } else if (duration === "2 hours") {
+    newDateObj = moment(startTime).add(120, "m").toDate();
+    return moment(newDateObj).format("h:mm a");
+  } else if (duration === "2 hours 30 min") {
+    newDateObj = moment(startTime).add(150, "m").toDate();
+    return moment(newDateObj).format("h:mm a");
+  } else if (duration === "3 hours") {
+    newDateObj = moment(startTime).add(180, "m").toDate();
+    return moment(newDateObj).format("h:mm a");
+  } else {
+    return newDateObj;
+  }
+};
